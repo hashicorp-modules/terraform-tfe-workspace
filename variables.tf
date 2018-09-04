d9b9243350af38ca0254528cb32e32f854dd0587
Jon Benson <bensojon@gmail.com>
2018-09-04 02:51:41 -0700

    Update repo

--- variables.tf
+++ variables.tf
@@ -10,7 +10,6 @@ variable "tfe_consumer_name"        { default = "tfe-workspace-consumer" }
 variable "tfe_consumer_team_id"     { default = "" }
 variable "tfe_consumer_team_access" { default = "read" }
 
-# variable "vcs_repo_identifier" { default = "hashicorp-modules/terraform-tfe-workspace" }
-variable "vcs_repo_identifier" { default = "hashicorp/terraform-guides" }
+variable "vcs_repo_identifier" { default = "hashicorp-modules/terraform-tfe-workspace" }
 variable "vcs_repo_branch"     { default = "master" }
 variable "working_directory"   { default = "./" }
